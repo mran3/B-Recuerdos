bbc8571834d1ca6ff258a21b3678b09c54fbf572
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2016-09-10 13:55:55 -0500

    Agregar librerias, test
    Añadi las librerias y lo probe

---
+++ build/generated-sources/ap-source-output/DataAccess/Entity/User_.java
@@ -0,0 +1,22 @@
+package DataAccess.Entity;
+
+import DataAccess.Entity.Account;
+import DataAccess.Entity.Session;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2016-09-10T13:13:57")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> firstName;
+    public static volatile SingularAttribute<User, String> lastName;
+    public static volatile CollectionAttribute<User, Account> accountCollection;
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, Integer> document;
+    public static volatile CollectionAttribute<User, Session> sessionCollection;
+    public static volatile SingularAttribute<User, String> userName;
+
+}
