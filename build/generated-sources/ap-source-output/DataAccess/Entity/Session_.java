faadd5c6b70fff9bc9d775eb6406f3c2dbe8a984
mran3 <x88andres@hotmail.com>
2016-09-11 18:51:37 -0500

    cambios de apariencia en index

--- build/generated-sources/ap-source-output/DataAccess/Entity/Session_.java
+++ build/generated-sources/ap-source-output/DataAccess/Entity/Session_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2016-09-11T00:14:07")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2016-09-11T14:01:44")
 @StaticMetamodel(Session.class)
 public class Session_ { 
 
--- build/generated-sources/ap-source-output/DataAccess/Entity/User_.java
+++ build/generated-sources/ap-source-output/DataAccess/Entity/User_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2016-09-11T00:14:07")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2016-09-11T14:01:44")
 @StaticMetamodel(User.class)
 public class User_ { 
 
