97584cbf2954d474261cb60240e7b94ee22ac79b
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2016-11-17 03:06:40 -0500

    SADASD
    ASDASD

--- src/java/BusinessLogic/Controller/LoginLdapController.java
+++ src/java/BusinessLogic/Controller/LoginLdapController.java
@@ -15,55 +15,6 @@ import java.io.UnsupportedEncodingException;
  */
 public class LoginLdapController {
 
-    private LDAPConnection lc = new LDAPConnection();
-
-    public String login(String user, String password){
-
-        if (connect()) {
-            if (validatePassword(user, password)) {
-                return "Your login was successful.";
-            } else {
-                return "Username or password is incorrect.";
-            }
-        } else {
-            return "Conection to LDAP server failed..";
-        }
-    }
-
-    public Boolean connect(){
-
-        String ldapHost = "127.0.0.1";
-        String dn = "cn=admin,dc=arqsoft,dc=unal,dc=edu,dc=co";
-        String password = "sa2016ii";
-
-        int ldapPort =  LDAPConnection.DEFAULT_PORT;
-        int ldapVersion = LDAPConnection.LDAP_V3;
-
-        try {
-            lc.connect(ldapHost, ldapPort);
-            System.out.println("==== Connecting to LDAP Server ====");
-            lc.bind(ldapVersion, dn, password.getBytes("UTF8"));
-            System.out.println("==== Authenticated in server ====");
-            return true;
-        } catch (LDAPException | UnsupportedEncodingException ex) {
-            System.out.println("==== ERROR to connecto to the LDAP Server ====");
-            return false;
-        }
-
-    }
-
-    public Boolean validatePassword(String user, String password){
-
-        String dn = "cn=" + user + ",ou=Bank,dc=arqsoft,dc=unal,dc=edu,dc=co";
-
-        try {
-            lc.bind(dn, password);
-            System.out.println("==== Password validated ====");
-            return true;
-        } catch (LDAPException ex) {
-            System.out.println("==== ERROR to validate the password ====");
-            return false;
-        }
-    }
+    
 }
 
--- src/java/BusinessLogic/Controller/UserController.java
+++ src/java/BusinessLogic/Controller/UserController.java
@@ -7,6 +7,9 @@ package BusinessLogic.Controller;
 
 import DataAccess.DAO.UserDAO;
 import DataAccess.Entity.User;
+import com.novell.ldap.LDAPConnection;
+import com.novell.ldap.LDAPException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import javax.faces.context.FacesContext;
 
@@ -18,7 +21,71 @@ public class UserController {
 
     public static final String USER_SESSION_KEY = "user";
 
-    
+    private LDAPConnection lc = new LDAPConnection();
+
+    public String loginUser(String user, String password){
+
+        if (connect()) {
+            if (validatePassword(user, password)) {
+                return "Your login was successful.";
+            } else {
+                return "Username or password is incorrect.";
+            }
+        } else {
+            return "Conection to LDAP server failed..";
+        }
+    }
+    /* 
+    public String loginUser(String userName, String password) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        UserDAO userDAO = new UserDAO();
+        User userLogin = userDAO.loginUser(userName);
+        if (userLogin != null) {
+            if (!userLogin.getPassword().equals(password)) {
+                return "err2";
+            }
+            context.getExternalContext().getSessionMap().put(USER_SESSION_KEY, userLogin);
+            return "main-index";
+        } else {
+            return "err1";
+        }
+    }
+    */
+    public Boolean connect(){
+
+        String ldapHost = "192.168.162.44";
+        String dn = "cn=admin,dc=arqsoft,dc=unal,dc=edu,dc=co";
+        String password = "sa2016ii";
+
+        int ldapPort =  LDAPConnection.DEFAULT_PORT;
+        int ldapVersion = LDAPConnection.LDAP_V3;
+
+        try {
+            lc.connect(ldapHost, ldapPort);
+            System.out.println("==== Connecting to LDAP Server ====");
+            lc.bind(ldapVersion, dn, password.getBytes("UTF8"));
+            System.out.println("==== Authenticated in server ====");
+            return true;
+        } catch (LDAPException | UnsupportedEncodingException ex) {
+            System.out.println("==== ERROR to connecto to the LDAP Server ====");
+            return false;
+        }
+
+    }
+
+    public Boolean validatePassword(String user, String password){
+
+        String dn = "cn=" + user + ",ou=Recuerdos,dc=arqsoft,dc=unal,dc=edu,dc=co";
+
+        try {
+            lc.bind(dn, password);
+            System.out.println("==== Password validated ====");
+            return true;
+        } catch (LDAPException ex) {
+            System.out.println("==== ERROR to validate the password ====");
+            return false;
+        }
+    }
     public String createUser(Integer id,
             String userName,
             String email,
@@ -56,21 +123,7 @@ public class UserController {
         }
     }
 
-    public String loginUser(String userName, String password) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        UserDAO userDAO = new UserDAO();
-        User userLogin = userDAO.loginUser(userName);
-        if (userLogin != null) {
-            if (!userLogin.getPassword().equals(password)) {
-                return "err2";
-            }
-            context.getExternalContext().getSessionMap().put(USER_SESSION_KEY, userLogin);
-            return "main-index";
-        } else {
-            return "err1";
-        }
-    }
-
+    
     public String updateUser(Integer id,
             String userName,
             String email,
