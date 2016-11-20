78f0b2dba2fe823e741a6e013272b1e1c53ad118
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2016-11-20 07:04:33 -0500

    Hacer roll back
    aa

--- src/java/BusinessLogic/Controller/UserController.java
+++ src/java/BusinessLogic/Controller/UserController.java
@@ -7,8 +7,6 @@ package BusinessLogic.Controller;
 
 import DataAccess.DAO.UserDAO;
 import DataAccess.Entity.User;
-import com.novell.ldap.LDAPConnection;
-import com.novell.ldap.LDAPException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import javax.faces.context.FacesContext;
@@ -21,8 +19,8 @@ public class UserController {
 
     public static final String USER_SESSION_KEY = "user";
 
-    private LDAPConnection lc = new LDAPConnection();
-
+    
+    /*
     public String loginUser(String user, String password){
 
         if (connect()) {
@@ -35,7 +33,7 @@ public class UserController {
             return "Conection to LDAP server failed..";
         }
     }
-    /* 
+    */ 
     public String loginUser(String userName, String password) {
         FacesContext context = FacesContext.getCurrentInstance();
         UserDAO userDAO = new UserDAO();
@@ -48,42 +46,6 @@ public class UserController {
             return "main-index";
         } else {
             return "err1";
-        }
-    }
-    */
-    public Boolean connect(){
-
-        String ldapHost = "192.168.162.44";
-        String dn = "cn=admin,dc=arqsoft,dc=unal,dc=edu,dc=co";
-        String password = "sa2016ii";
-
-        int ldapPort =  LDAPConnection.DEFAULT_PORT;
-        int ldapVersion = LDAPConnection.LDAP_V3;
-
-        try {
-            lc.connect(ldapHost, ldapPort);
-            System.out.println("==== Connecting to LDAP Server ====");
-            lc.bind(ldapVersion, dn, password.getBytes("UTF8"));
-            System.out.println("==== Authenticated in server ====");
-            return true;
-        } catch (LDAPException | UnsupportedEncodingException ex) {
-            System.out.println("==== ERROR to connecto to the LDAP Server ====");
-            return false;
-        }
-
-    }
-
-    public Boolean validatePassword(String user, String password){
-
-        String dn = "cn=" + user + ",ou=Recuerdos,dc=arqsoft,dc=unal,dc=edu,dc=co";
-
-        try {
-            lc.bind(dn, password);
-            System.out.println("==== Password validated ====");
-            return true;
-        } catch (LDAPException ex) {
-            System.out.println("==== ERROR to validate the password ====");
-            return false;
         }
     }
     public String createUser(Integer id,
