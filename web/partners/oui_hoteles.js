6435cf2f0c70aa78ec9ff99e174e4cc365c54937
mran3 <x88andres@hotmail.com>
2016-11-20 06:52:29 -0500

    se avanzó lo que se pudo con la info disponible para la interconexion rest con los otros grupos.

---
+++ web/partners/oui_hoteles.js
@@ -0,0 +1,79 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var oModel = new sap.ui.model.json.JSONModel();
+
+/***** LOADING THE DATA *****/
+oModel.loadData('/A-Restaurantes/rest/restaurants');
+
+//var data =
+//        [
+//            {"name": "Rajesh"},
+//            {"name": "Kunal Jauhari"},
+//            {"name": "Ashish Singh"},
+//            {"name": "Ansuman Parhi"},
+//            {"name": "Arup Kumar"},
+//            {"name": "Deepak Malviya"},
+//            {"name": "Seshu"},
+//            {"name": "Ankush Datey"},
+//            {"name": "Tapesh Syawaria"},
+//            {"name": "Mahesh"},
+//            {"name": "Vinay Joshi"},
+//            {"name": "Ardhendu Karna"},
+//            {"name": "Abhishek Shukla"},
+//            {"name": "Kashim"},
+//            {"name": "Vinayak"}
+//        ]
+//        ;
+
+
+
+// register model in core
+sap.ui.getCore().setModel(oModel);
+
+// create your table
+var oTable1 = new sap.ui.table.Table({
+    title: "Restaurantes",
+    visibleRowCount: 4,
+    selectionMode: sap.ui.table.SelectionMode.Single,
+    navigationMode: sap.ui.table.NavigationMode.Paginator,
+    // bind the core-model to this table by aggregating player-Array
+    rows: '{/}'
+});
+
+// define the columns and the control templates to be used    
+oTable1.addColumn(new sap.ui.table.Column({
+    label: new sap.ui.commons.Label({
+        text: "Nombre"
+    }),
+    template: new sap.ui.commons.TextView({
+        text: '{name}'
+    }),
+    width: "10px"
+}));
+
+oTable1.addColumn(new sap.ui.table.Column({
+    label: new sap.ui.commons.Label({
+        text: "Dirección"
+    }),
+    template: new sap.ui.commons.TextView({
+        text: '{addres}'
+    }),
+    width: "10px"
+}));
+
+oTable1.addColumn(new sap.ui.table.Column({
+    label: new sap.ui.commons.Label({
+        text: "Teléfono"
+    }),
+    template: new sap.ui.commons.TextView({
+        text: '{phone}'
+    }),
+    width: "10px"
+}));
+
+//place at DOM
+oTable1.placeAt('prueba');
